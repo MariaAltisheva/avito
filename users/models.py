d13783ccd05302c1781ab256b945f198d2700048
MariaAltysheva <altysheva_maria@mail.ru>
2022-10-26 14:56:02 +0200

    changes class User

--- users/models.py
+++ users/models.py
@@ -14,10 +14,29 @@ class Location(models.Model):
         return self.name
 
 
+class UserRoles:
+    MEMBER = 'member'
+    MODERATOR = 'moderator'
+    ADMIN = 'admin'
+    choices = ((MEMBER, 'Пользователь'),
+               (ADMIN, 'Администратор'),
+               (MODERATOR, 'Модератор'))
+
+
+
+
 class User(models.Model):
-    first_name = models.CharField(verbose_name='Имя', max_length=100)
-    last_name = models.CharField(verbose_name='Фамилия', max_length=200)
+    first_name = models.CharField(verbose_name='Имя', max_length=100, null=True)
+    last_name = models.CharField(verbose_name='Фамилия', max_length=200, null=True)
     username = models.CharField(verbose_name='Логин', max_length=200, unique=True)
     password = models.CharField(verbose_name='Пароль', max_length=200)
     age = models.PositiveSmallIntegerField()
     location = models.ManyToManyField(Location)
+    role = models.CharField(choices=UserRoles.choices, default=UserRoles.MEMBER, max_length=10)
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
