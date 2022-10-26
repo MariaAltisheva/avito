811729e9f918a1ab73e4d9b5b69a5063ba9aa371
MariaAltysheva <altysheva_maria@mail.ru>
2022-10-26 14:09:31 +0200

    added datasets

--- avito/settings.py
+++ avito/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'avito.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'PORT': '5432',
+        'HOST': 'localhost'
     }
 }
 
