ab41e2f2a87291b4daa1e550e6136c7e30237fe6
MariaAltysheva <altysheva_maria@mail.ru>
2022-10-26 14:23:02 +0200

    change models

--- ads/models.py
+++ ads/models.py
@@ -1,17 +1,25 @@
 from django.db import models
 
 
-class Ad(models.Model):
-    name = models.CharField(max_length=150, unique=True)
-    author = models.CharField(max_length=50)
-    price = models.PositiveIntegerField()
-    description = models.CharField(max_length=300)
-    address = models.CharField(max_length=200)
-    is_published = models.BooleanField(default=True)
-
-
 class Category(models.Model):
     name = models.CharField(max_length=100, unique=True)
+
+class Ad(models.Model):
+    name = models.CharField(max_length=150, unique=True)
+    author = models.ForeignKey(User, verbose_name='Автор', related_name='ads', on_delete=models.CASCADE)
+    price = models.PositiveIntegerField()
+    description = models.CharField(max_length=300)
+    is_published = models.BooleanField(default=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Объявление'
+        verbose_name_plural = 'Объявления'
+
+    def __str__(self):
+        return self.name
+
+
 
 
 
