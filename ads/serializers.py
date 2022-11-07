ed2866472d29a4de43628eb9df47a9d5c243535b
MariaAltysheva <altysheva_maria@mail.ru>
2022-11-07 09:43:51 +0200

    serializers

--- ads/serializers.py
+++ ads/serializers.py
@@ -20,7 +20,6 @@ class AdListSerializer(serializers.ModelSerializer):
 
 class AdDetailSerializer(serializers.ModelSerializer):
     author = UserAdSerializer
-    # author = SlugRelatedField(slug_field='username', queryset=User.objects.all())
     category = SlugRelatedField(slug_field='name', queryset=Category.objects.all())
     class Meta:
         model = Ad
--- users/serializers.py
+++ users/serializers.py
@@ -17,6 +17,24 @@ class UserCreateSerializer(serializers.ModelSerializer):
             user.location.add(location)
         return user
 
+    class Meta:
+        model = User
+        fields = "__all__"
+
+
+class UserUpdateSerializer(serializers.ModelSerializer):
+    location = serializers.SlugRelatedField(required=False, queryset=Location.objects.all(), many=True, slug_field="name")
+
+    def is_valid(self, *, raise_exception=False):
+        self._locations = self.initial_data.pop('location', [])
+        return super().is_valid(raise_exception=raise_exception)
+
+    def save(self, **kwargs):
+        user = super().save(**kwargs)
+        for loc_name in self._locations:
+            location, _ = Location.objects.get_or_create(name=loc_name)
+            user.location.add(location)
+        return user
 
     class Meta:
         model = User
@@ -27,15 +45,24 @@ class UserAdSerializer(serializers.ModelSerializer):
         model = User
         fields = ['first_name', 'last_name', 'username']
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
 
 
+class UserListSerializer(serializers.ModelSerializer):
+    total_ads = serializers.SerializerMethodField()
 
+    class Meta:
+        model = User
+        fields = '__all__'
+    def get_total_ads(self, user):
+        return user.ad_set.filter(is_published=True).count()
 
 
 
 class LocationSerializer(serializers.ModelSerializer):
-
-
     class Meta:
         model = Location
         fields = "__all__"
